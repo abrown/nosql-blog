67af1d5ac7f802a27ae13a41b625310f2512bd39
Andrew Brown <andrew@casabrown.com>
2011-11-26 19:53:06 -0500

    Added XSS protection

--- classes/Post.php
+++ classes/Post.php
@@ -1,8 +1,20 @@
 <?php
+/**
+ * Blog Posts
+ */
 class Post extends ServiceObjectItem{
     
+    /**
+     * Creates records
+     * @param stdClass $item
+     * @return int 
+     */
     public function create($item = null){
+        // add times
         if( !is_null($item) ) $item->created = date('Y-m-d H:i:s');
+        // clean XSS
+        $item = WebHttp::clean($item, 'html');
+        // create new item
         $id = parent::create($item);
         // redirect
         $_GET['message'] = 'Item created successfully';
@@ -12,8 +24,17 @@ class Post extends ServiceObjectItem{
         return $id;
     }
     
+    /**
+     * Updates records
+     * @param stdClass $item
+     * @return stdClass 
+     */
     public function update($item = null){
+        // add times
         if( !is_null($item) ) $item->modified = date('c');
+        // clean XSS
+        $item = WebHttp::clean($item, 'html');
+        // update item
         $item = parent::update($item);
         // redirect
         $_GET['message'] = 'Item updated successfully';
@@ -23,6 +44,10 @@ class Post extends ServiceObjectItem{
         return $item;
     }
     
+    /**
+     * Deletes records
+     * @return stdClass 
+     */
     public function delete(){
         $item = parent::delete();
         // redirect
